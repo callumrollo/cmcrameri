fb82334d3531d6e41d029ab66fecb43387826a40
Callum Rollo <c.rollo@outlook.com>
2021-02-02 11:45:11 +0000

    linted tests

--- tests/test_cmcrameri.py
+++ tests/test_cmcrameri.py
@@ -1,14 +1,14 @@
 """
 Test that the program a) finds the text files and b) creates colourmaps
 """
-
-import matplotlib
+from matplotlib.colors import LinearSegmentedColormap
 from pathlib import Path
 import sys
+
 library_dir = Path(__file__).parent.parent.absolute()
-print(library_dir)
 sys.path.append(str(library_dir))
 from cmcrameri import cm
+
 
 def test_find_files():
     assert len(cm.paths) > 0
@@ -21,13 +21,14 @@ def test_cmap_import():
     for name, cmap in vars(cm).items():
         increment = 1
         # See if it is a colormap.
-        if isinstance(cmap, matplotlib.colors.LinearSegmentedColormap):
+        if isinstance(cmap, LinearSegmentedColormap):
             if name[-1] != 'S':
                 increment = 0.5
             no_cmaps += increment
             cmap_names.append(name)
     # Should be as many colour maps as files plus reversed for non categorical ones
     assert int(no_cmaps) == len(cm.paths)
+
 
 test_find_files()
 test_cmap_import()
