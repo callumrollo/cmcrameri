d1fc05b6a73aae5abe201d3b95dde8fe3ed8c84a
Callum Rollo <c.rollo@outlook.com>
2021-10-11 18:21:47 +0100

    using gliderpy methods for setup

--- cmcrameri/__init__.py
+++ cmcrameri/__init__.py
@@ -1,5 +1,5 @@
 """
-cmcrameri is a package of perceptuallt uniform colourmaps for the geosciences
+cmcrameri is a package of perceptually uniform colourmaps for the geosciences
 This is mererly a wrapper for previosuly created colour maps. All credit to Fabio Crameri
 http://www.fabiocrameri.ch/colourmaps.php
 See README.md for an overview and instructions
@@ -11,6 +11,9 @@ from . import cm
 
 __authors__ = ['Callum Rollo <c.rollo@outlook.com>']
 
-__version__ = "1.2"
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
 
 __scm_version__ = "7.0"
--- setup.py
+++ setup.py
@@ -1,32 +1,10 @@
-"""
-setup.py for cmcrameri
+from setuptools import setup
 
-"""
-from setuptools import setup 
-
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 setup(
-  name = 'cmcrameri',        
-  packages = ['cmcrameri'],  
-  version = '1.2',
-  license='MIT',   
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  description = 'Perceptually uniform colourmaps',   
-  author = 'Callum Rollo',         
-  author_email = 'c.rollo@outlook.com',      
-  url = 'https://github.com/callumrollo/cmcrameri',  
-  download_url = 'https://github.com/callumrollo/cmcrameri/v_01.tar.gz',  
-  keywords = ['colormaps', 'oceanography', 'plotting', 'visualization'],
-  install_requires=[         
-          'matplotlib',
-          'numpy',
-      ],
-  package_dir={'mypkg': 'cmcrameri'},     
-  package_data={'cmcrameri': ['cmaps/*.txt']},
-  data_files=[("",["LICENSE"])]
+    use_scm_version={
+        "write_to": "cmcrameri/_version.py",
+        "write_to_template": '__version__ = "{version}"',
+        "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
+    }
 )
